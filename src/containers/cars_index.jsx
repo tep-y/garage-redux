25fd7ac80e8966d73934dcd2332b959e057259f3
tep yith <tep.yith@gmail.com>
2021-06-04 15:26:23 -0400

    Able to display car array info.

--- src/containers/cars_index.jsx
+++ src/containers/cars_index.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from  'react';
+// import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 class CarsIndex extends Component {
   render () {
     return (
       <div>
-
+        {
+        this.props.cars.map((car) => {
+          return car.brand
+        })
+        }
       </div>
     );
   }
 }
 
-export default CarsIndex;
+function mapStateToProps(state) {
+  return {
+    cars: state.cars
+  }
+}
+
+export default connect(mapStateToProps)(CarsIndex);
--- src/index.jsx
+++ src/index.jsx
@@ -33,7 +33,7 @@ const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={createStore(reducers, initialState, middlewares)}>
     <Router history={history}>
       <Switch>
         <Route component={CarsIndex}/>
